9e7b439e34046f6d3320c3d83e9895c19fc2b34f
Leslie <lesles08@gmail.com>
2026-02-24 21:24:36 -0600

    app

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import Header from "./../components/Header";
+import type { Metadata } from "next";
+import { Playfair_Display, Inter } from 'next/font/google'
+import "./globals.css";
+
+const playfair = Playfair_Display({ subsets: ['latin'], variable: '--font-playfair' })
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
+
+
+export const metadata: Metadata = {
+  title: "Honor Gift",
+  description: "Honor Gift description",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" className={`${playfair.variable} ${inter.variable}`}>
+      <body className="antialiased font-sans">
+        <Header /> {/* Add the Header here! */}
+        {children}
+      </body>
+    </html>
+  )
+}
